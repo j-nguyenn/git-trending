daae1e76f4f9e5bb829093791680b08d09267dd2
jng <jasminee.ngx@gmail.com>
2021-08-30 08:44:45 +0100

    Update footer

--- src/components/footer/footer.component.tsx
+++ src/components/footer/footer.component.tsx
@@ -26,7 +26,7 @@ export const FooterComponent = () => {
       <img src="https://media0.giphy.com/media/mVJ5xyiYkC3Vm/giphy.gif?cid=790b7611ad2812b1ccf7f99cf2e9764d8ac87928c092140a&rid=giphy.gif" />
       <small>
         © 2021 by
-        <a href="https://huocha.github.io/git-trending/">JASMINE NG </a>
+        <a href="https://j-nguyenn.github.io/git-trending/">JASMINE NG </a>
       </small>
     </footer>
   );
